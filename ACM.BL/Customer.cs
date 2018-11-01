63b3c7da960b0e3c94a827ed470810b7d2bff0e3
Mark Huntjens <mark_huntjens@reyrey.com>
2018-11-01 08:25:34 +0100

    AddRepositoryClassesAndEmailLibrary

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -8,5 +8,9 @@ namespace ACM.BL
 {
     public class Customer
     {
+        public int CustomerId { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }

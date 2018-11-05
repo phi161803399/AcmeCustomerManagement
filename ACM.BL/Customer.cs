ef8e64df8f595b83288afd2cba3734b42dda99e9
Mark Huntjens <mark_huntjens@reyrey.com>
2018-11-05 08:31:22 +0100

    FixedBuggInCalculatePercentOfGoalSteps

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -29,10 +29,14 @@ namespace ACM.BL
             if (string.IsNullOrWhiteSpace(actualSteps)) throw new ArgumentException("Actual steps must be entered", "actualSteps");
 
             if (!decimal.TryParse(goalSteps, out goalStepCount)) throw new ArgumentException("Goal must be numeric", "goalStepCount");
-            if (!decimal.TryParse(goalSteps, out actualStepCount)) throw new ArgumentException("Actual step must be numeric", "actualStepCount");
+            if (!decimal.TryParse(actualSteps, out actualStepCount)) throw new ArgumentException("Actual step must be numeric", "actualStepCount");
 
+            return CalculatePercentOfGoalSteps(goalStepCount, actualStepCount);
+        }
+
+        public decimal CalculatePercentOfGoalSteps(decimal goalStepCount, decimal actualStepCount)
+        {
             if (goalStepCount <= 0) throw new ArgumentException("Goal must be greater than 0", "goalSteps");
-
             return (actualStepCount / goalStepCount) * 100;
         }
     }

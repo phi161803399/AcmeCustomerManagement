4abd4f17136ca377e577e19318778bb23554b83c
Mark Huntjens <mark_huntjens@reyrey.com>
2018-11-06 08:12:13 +0100

    AddUnitTestForCustomer

---
+++ ACM.BLTests/CustomerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValid()
+        {
+            // Arrange
+            var customer = new Customer();
+            var goalSteps = "5000";
+            var actualSteps = "2000";
+            var expected = 40M;
+
+            // Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValidAndSame()
+        {
+            // Arrange
+            var customer = new Customer();
+            var goalSteps = "5000";
+            var actualSteps = "5000";
+            var expected = 100M;
+
+            // Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValidAndActualIsZero()
+        {
+            // Arrange
+            var customer = new Customer();
+            var goalSteps = "5000";
+            var actualSteps = "0";
+            var expected = 0M;
+
+            // Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

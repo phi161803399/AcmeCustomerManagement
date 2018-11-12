47830e3d09b1f217a54dc2c562136ddf0e6e1c5e
Mark <Mark@DESKTOP-16DN5B4>
2018-11-12 21:34:06 +0100

    AddSwitchAndEnumToPayment

--- ACM.BL/Payment.cs
+++ ACM.BL/Payment.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
+    public enum PaymentType
+    {
+        CreditCard = 1,
+        PayPal = 2
+    }
+
     public class Payment
     {
+        public int PaymentType { get; set; }
         public void ProcessPayment()
         {
-            throw new NotImplementedException();
+            PaymentType paymentTypeOption;
+            if (!enum.TryParse(this.PaymentType.ToString(), out paymentTypeOption)
+            {
+                throw new InvalidEnumArgumentException("Payment Type", (int)this.PaymentType, typeof(PaymentType));
+            }
+
+            switch (paymentTypeOption)
+            {
+                case ACM.BL.PaymentType.CreditCard:
+                    // Process credit card
+                    break;
+                case ACM.BL.PaymentType.PayPal:
+                    // Process PayPal
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
         }
     }
 }
